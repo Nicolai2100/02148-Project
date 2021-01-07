ed46e6efaad6b1055009f76c35087ed4a565faf8
Gustav <gustav.nobert1998@gmail.com>
2021-01-07 14:30:01 +0100

    you can now make calls to the API to gather the historical data of a stock

--- src/main/java/BeastProject/yahooAPI/APIcalls.java
+++ src/main/java/BeastProject/yahooAPI/APIcalls.java
@@ -3,76 +3,118 @@ package BeastProject.yahooAPI;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.apache.tomcat.util.json.JSONParser;
-import org.apache.tomcat.util.json.ParseException;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.jspace.SequentialSpace;
+import org.jspace.SpaceRepository;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class APIcalls {
     private static final String apikey = "9ef62d9bdcmshfef91e2b707c69cp1724fejsncd7e1e4f3845";
     private static final String apihost = "apidojo-yahoo-finance-v1.p.rapidapi.com";
+    private static SpaceRepository stockRepository;
+
+    public APIcalls () { stockRepository = new SpaceRepository(); }
 
     public static void main(String[] args) {
-        try {
-            HttpResponse<String> response = Unirest.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=AMRN&region=US")
-                    .header("x-rapidapi-key", "9ef62d9bdcmshfef91e2b707c69cp1724fejsncd7e1e4f3845")
-                    .header("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
-                    .asString();
-
-            response.getBody();
-
-            JSONObject jsonObject = new JSONObject(response.getBody());
-            JSONArray myResponse = jsonObject.getJSONArray("prices");
-            ArrayList<StockModel> stockModels = new ArrayList<>();
-
-            for (int i = 0; i < myResponse.length(); i++) {
-                JSONObject jsonObject1 = myResponse.getJSONObject(i);
-                Date date1 = new Date();
-                date1.setTime((int) jsonObject1.get("date"));
-                StockModel stockModel = new StockModel(date1, (int) jsonObject1.get("volume"), (double) jsonObject1.get("high"), (double) jsonObject1.get("low"), (double) jsonObject1.get("adjclose"), (double) jsonObject1.get("close"), (double) jsonObject1.get("open"));
-                stockModels.add(stockModel);
-            }
-            System.out.println(stockModels);
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
+        // And then we ofc need to add it with the same handle. The handle and region needs to come from a user.
+        // The name is made op of the handle/region.
+        APIcalls apicalls = new APIcalls();
+        getStockRepository().add("ARMN/US", apicalls.gethistoricaldata("AMRN","US"));
+        getStockRepository().add("TSLA/US",apicalls.findStock("TSLA","US"));
+        System.out.println("hej");
     }
 
     /**
+     * The api will return a datatype of either double or integer depending on the amount of zeros.
+     * We therefore need to check if the type is integer, and if so cast it to a double...
      * @param region the region in which the company is, f.eks TSLA is in the US
      * @param handle The handle for the company, it is the abbreviation which for tesla is TSLA for example.
      */
-    public static ArrayList<StockModel> gethistoricaldata(String region, String handle) {
+    public SequentialSpace gethistoricaldata(String handle, String region) {
+        SequentialSpace sequentialSpace = new SequentialSpace();
         try {
-            HttpResponse<String> response = Unirest.get
-                    ("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=" + handle + "&region=" + region)
-                    .header("x-rapidapi-key", "9ef62d9bdcmshfef91e2b707c69cp1724fejsncd7e1e4f3845")
-                    .header("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
-                    .asString();
+            String requestaddress = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=" + handle + "&region=" + region;
+            HttpResponse<String> response = Unirest.get(requestaddress)
+                    .header("x-rapidapi-key", apikey).header("x-rapidapi-host", apihost).asString();
 
-            response.getBody();
 
             JSONObject jsonObject = new JSONObject(response.getBody());
             JSONArray myResponse = jsonObject.getJSONArray("prices");
-            ArrayList<StockModel> stockModels = new ArrayList<>();
 
             for (int i = 0; i < myResponse.length(); i++) {
                 JSONObject jsonObject1 = myResponse.getJSONObject(i);
                 Date date1 = new Date();
+                // There is a need to find out if the line contains the correct fields. In case of a split happening for a stock
+                // A new entry will be added to the HttpResponse with different fields.
+                if (jsonObject1.has("volume")) {
                 date1.setTime((int) jsonObject1.get("date"));
-                StockModel stockModel = new StockModel(date1, (int) jsonObject1.get("volume"), (double) jsonObject1.get("high"), (double) jsonObject1.get("low"), (double) jsonObject1.get("adjclose"), (double) jsonObject1.get("close"), (double) jsonObject1.get("open"));
-                stockModels.add(stockModel);
+                int volume = (int) jsonObject1.get("volume");
+                double high, low, adjclose, close, open;
+                // There is a need to check which type it is. If the stock prize doesnt have any decimals the api returns
+                // The datatype integer instead of double, and therefore we get this clustered code.
+                if (myResponse.getJSONObject(i).get("high").getClass() == Integer.class) {
+                    high = (int) myResponse.getJSONObject(i).get("high");
+                } else {
+                    high = (double) myResponse.getJSONObject(i).get("high");
+                }
+
+                if (myResponse.getJSONObject(i).get("low").getClass() == Integer.class) {
+                    low = (int) myResponse.getJSONObject(i).get("low");
+                } else {
+                    low = (double) myResponse.getJSONObject(i).get("low");
+                }
+
+                if (myResponse.getJSONObject(i).get("adjclose").getClass() == Integer.class) {
+                    adjclose = (int) myResponse.getJSONObject(i).get("adjclose");
+                } else {
+                    adjclose = (double) myResponse.getJSONObject(i).get("adjclose");
+                }
+
+                if (myResponse.getJSONObject(i).get("close").getClass() == Integer.class) {
+                    close = (int) myResponse.getJSONObject(i).get("close");
+                } else {
+                    close = (double) myResponse.getJSONObject(i).get("close");
+                }
+
+                if (myResponse.getJSONObject(i).get("open").getClass() == Integer.class) {
+                    open = (int) myResponse.getJSONObject(i).get("open");
+                } else {
+                    open = (double) myResponse.getJSONObject(i).get("open");
+                }
+                StockModel stockModel = new StockModel(date1, volume, high, low, adjclose, close, open);
+                try {
+                    sequentialSpace.put(stockModel);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            System.out.println(stockModels);
-            return stockModels;
+            }
+            return sequentialSpace;
         } catch (UnirestException e) {
             e.printStackTrace();
         }
-        return new ArrayList<>();
+        return sequentialSpace;
+    }
+
+    /**
+     * This returns a stock. It either finds the stock in the already gathered stocks or queries the API.
+     * @param handle
+     * @param region
+     * @return
+     */
+    public SequentialSpace findStock(String handle, String region) {
+        SequentialSpace sequentialSpace = (SequentialSpace) stockRepository.get(handle + "/" + region);
+        if (sequentialSpace == null) {
+            return gethistoricaldata(handle, region);
+        } else {
+            getStockRepository().add(handle + "/" + region,sequentialSpace);
+            return sequentialSpace;
+        }
+    }
+
+    public static SpaceRepository getStockRepository() {
+        return stockRepository;
     }
 }
